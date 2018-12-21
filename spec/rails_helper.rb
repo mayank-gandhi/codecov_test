b8620f471212bf581d43c4f125192d7cab0b7836
Mayank Gandhi <mayank.gandhi@gemsessence.com>
2018-12-21 13:16:30 +0530

    Added simplecov profile configuration in rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,8 +10,19 @@ require 'factory_bot_rails'
 require 'shoulda/matchers'
 
 require 'simplecov'
+SimpleCov.profiles.define 'codecov-test' do
+  load_profile 'rails' # simplecov defaults
 
-SimpleCov.start do
+  add_filter 'spec'
+  add_filter '/config/'
+
+  add_group 'Controllers', 'app/controllers'
+  add_group 'Models', 'app/models'
+  add_group 'Helpers', 'app/helpers'
+  add_group 'Libraries', 'lib'
+end
+
+SimpleCov.start 'codecov-test' do
   add_group "Models", "app/models"
   add_group "Controllers", "app/controllers"
   add_group "Multiple Files", ["app/models", "app/controllers"] # You can also pass in an array
